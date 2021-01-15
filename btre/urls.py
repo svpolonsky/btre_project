0ddc14ae87e8a9df3a571b833c1fe147ac35f795
Stanislav Polonsky <spolonsky@icloud.com>
2021-01-15 12:49:52 +0300

    verification for Yandex

--- btre/urls.py
+++ btre/urls.py
@@ -5,6 +5,7 @@ from .sitemap import sitemaps
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('',include('pages.urls')),
@@ -16,5 +17,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
     path('sitemap.xml', sitemap,{'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("yandex_748235101bd5901c.html",TemplateView.as_view(template_name="yandex_748235101bd5901c.html", content_type="text/html")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
